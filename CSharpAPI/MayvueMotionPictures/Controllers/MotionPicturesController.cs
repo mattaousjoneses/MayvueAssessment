5922f188564272ed5f27d9374d443d9f2faf1383
Matthew Jones <mattaousjoneses@gmail.com>
2021-08-04 20:11:20 -0400

    Add files via upload

--- CSharpAPI/MayvueMotionPictures/Controllers/MotionPicturesController.cs
+++ CSharpAPI/MayvueMotionPictures/Controllers/MotionPicturesController.cs
@@ -4,22 +4,23 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
-
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.Web.Http.Cors;
 
 namespace MayvueMotionPictures.Controllers
 {
+    //[EnableCors]
     [Route("api/[controller]")]
     [ApiController]
     public class MotionPicturesController : ControllerBase
     {
         // GET: api/<MotionPicturesController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult Get()
         {
             string toReturn = string.Empty;
-            
+
             string queryString = "SELECT *  FROM  MotionPictures";
             string connectionString = "Server=(local);Database=MotionPictureDB;";
 
@@ -32,11 +33,7 @@ namespace MayvueMotionPictures.Controllers
                 List<Object> dataReturn = new System.Collections.Generic.List<Object>();
                 try
                 {
-                    while (reader.Read())
-                    {
-                        Console.WriteLine(String.Format("ID = {0}, Title = {1}",
-                        reader["ID"], reader["Title"]));// etc
-                    }
+                    toReturn = sqlDataToJson(reader);
                 }
                 finally
                 {
@@ -44,7 +41,8 @@ namespace MayvueMotionPictures.Controllers
                     reader.Close();
                 }
             }
-            return new string[] { "value1", "value2" };
+
+            return Ok(toReturn);
         }
 
         // GET api/<MotionPicturesController>/5
@@ -58,18 +56,66 @@ namespace MayvueMotionPictures.Controllers
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            var MotionPicture = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(value);
+
+            string title = MotionPicture.Title;
+            string desc = MotionPicture.Description;
+            string rY = MotionPicture.ReleaseYear;
+
+            string queryString = $"INSERT INTO MotionPictures VALUES ('{title}', '{desc}', '{rY}') GO";
+            string connectionString = "Server=(local);Database=MotionPictureDB;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //command.Parameters.AddWithValue("@tPatSName", "Your-Parm-Value");
+                connection.Open();
+                command.ExecuteReader();
+                connection.Close();
+            }
+
         }
 
         // PUT api/<MotionPicturesController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            string title="", desc="", rY="";
+
+            var MotionPicture = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(value);
+
+            title = MotionPicture.Title;
+            desc = MotionPicture.Description;
+            rY = MotionPicture.ReleaseYear;
+
+            string queryString = $"UPDATE MotionPictures SET Title='{title}', Description='{desc}', ReleaseYear='{rY}' WHERE id={id} GO" ;
+            string connectionString = "Server=(local);Database=MotionPictureDB;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //command.Parameters.AddWithValue("@tPatSName", "Your-Parm-Value");
+                connection.Open();
+                command.ExecuteReader();
+                connection.Close();
+            }
         }
 
         // DELETE api/<MotionPicturesController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            string queryString = "DELETE FROM MotionPictures WHERE id="+id+" GO";
+            string connectionString = "Server=(local);Database=MotionPictureDB;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                //command.Parameters.AddWithValue("@tPatSName", "Your-Parm-Value");
+                connection.Open();
+                command.ExecuteReader();
+                connection.Close();
+            }
         }
 
         private String sqlDataToJson(SqlDataReader dataReader)
@@ -80,54 +126,7 @@ namespace MayvueMotionPictures.Controllers
             JSONString = JsonConvert.SerializeObject(dataTable);
             return JSONString;
         }
+
+
     }
 }
-
-/*
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MayvueMotionPictures.Properties
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MotionPicturesController : ControllerBase
-    {
-        // GET: api/<MotionPicturesController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<MotionPicturesController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<MotionPicturesController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<MotionPicturesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<MotionPicturesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-    }
-}
-*/
